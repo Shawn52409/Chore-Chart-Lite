432839d28b9423d1f150a9c448e8a7140f999ef3
Shawn Miller <shawnsmm@gmail.com>
2022-03-23 15:29:51 -0400

    WIP

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,19 @@ var currentDate = moment().format('dddd (l)');
 // function called after a search is made
 function startCoinCeremony(evt){    
     evt.preventDefault();
-    console.log(evt);
+    
+    var results = {};
 
-}
+    // for(i=0;i<18;i++){
+    
+    
+    // var ${`chore1CheckBox${i}`} = $(`#chore${i}`).val();
+    // var ${`caseyCompletedChore${i}`} = $(`#caseyCompletedChore${i}`).val();
+    // var ${`caseyCompletedChore${i}`} = $(`#caseyCompletedChore${i}`).val();
+
+    // }
+
+};
 
 
 // Search button handler
