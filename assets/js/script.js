fa3d122fcd8349b93b875604af19b4164531907b
Shawn Miller <shawnsmm@gmail.com>
2022-03-24 07:46:10 -0400

    WIP

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,64 @@ function startCoinCeremony(evt){
     evt.preventDefault();
     
     var results = {};
+    var chore1 = $("#chore1").val();
+    var caseyCompletedChore1 = $("#caseyCompletedChore1").val();
+    var connorCompletedChore1 = $("#connorCompletedChore1").val();
+    var chore2 = $("#chore2").val();
+    var caseyCompletedChore2 = $("#caseyCompletedChore2").val();
+    var connorCompletedChore2 = $("#connorCompletedChore2").val();
+    var chore3 = $("#chore3").val();
+    var caseyCompletedChore3 = $("#caseyCompletedChore3").val();
+    var connorCompletedChore3 = $("#connorCompletedChore3").val();
+    var chore4 = $("#chore4").val();
+    var caseyCompletedChore4 = $("#caseyCompletedChore4").val();
+    var connorCompletedChore4 = $("#connorCompletedChore4").val();
+    var chore5 = $("#chore5").val();
+    var caseyCompletedChore5 = $("#caseyCompletedChore5").val();
+    var connorCompletedChore5 = $("#connorCompletedChore5").val();
+    var chore6 = $("#chore6").val();
+    var caseyCompletedChore6 = $("#caseyCompletedChore6").val();
+    var connorCompletedChore6 = $("#connorCompletedChore6").val();
+    var chore1 = $("#chore1").val();
+    var caseyCompletedChore1 = $("#caseyCompletedChore1").val();
+    var connorCompletedChore1 = $("#connorCompletedChore1").val();
+    var chore1 = $("#chore1").val();
+    var caseyCompletedChore1 = $("#caseyCompletedChore1").val();
+    var connorCompletedChore1 = $("#connorCompletedChore1").val();
+    var chore1 = $("#chore1").val();
+    var caseyCompletedChore1 = $("#caseyCompletedChore1").val();
+    var connorCompletedChore1 = $("#connorCompletedChore1").val();
+    var chore1 = $("#chore1").val();
+    var caseyCompletedChore1 = $("#caseyCompletedChore1").val();
+    var connorCompletedChore1 = $("#connorCompletedChore1").val();
+    var chore1 = $("#chore1").val();
+    var caseyCompletedChore1 = $("#caseyCompletedChore1").val();
+    var connorCompletedChore1 = $("#connorCompletedChore1").val();
+    var chore1 = $("#chore1").val();
+    var caseyCompletedChore1 = $("#caseyCompletedChore1").val();
+    var connorCompletedChore1 = $("#connorCompletedChore1").val();
+    var chore1 = $("#chore1").val();
+    var caseyCompletedChore1 = $("#caseyCompletedChore1").val();
+    var connorCompletedChore1 = $("#connorCompletedChore1").val();
+    var chore1 = $("#chore1").val();
+    var caseyCompletedChore1 = $("#caseyCompletedChore1").val();
+    var connorCompletedChore1 = $("#connorCompletedChore1").val();
+    var chore1 = $("#chore1").val();
+    var caseyCompletedChore1 = $("#caseyCompletedChore1").val();
+    var connorCompletedChore1 = $("#connorCompletedChore1").val();
+    var chore1 = $("#chore1").val();
+    var caseyCompletedChore1 = $("#caseyCompletedChore1").val();
+    var connorCompletedChore1 = $("#connorCompletedChore1").val();
+    var chore1 = $("#chore1").val();
+    var caseyCompletedChore1 = $("#caseyCompletedChore1").val();
+    var connorCompletedChore1 = $("#connorCompletedChore1").val();
+    var chore1 = $("#chore1").val();
+    var caseyCompletedChore1 = $("#caseyCompletedChore1").val();
+    var connorCompletedChore1 = $("#connorCompletedChore1").val();
+
+
+
+
 
     // for(i=0;i<18;i++){
     
